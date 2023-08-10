fffb502f1e814db61f5ec9ed21a95f5c48ed7a3b
sc <sceland940403@gmail.com>
2023-08-10 14:37:05 +0900

    추가작업

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ const Btn = styled.button`
 function App() {
   return (
     <div className="App">
+      <Btn>버튼</Btn>
       <div>헤더작업</div>
+      <div>작업하나 추가</div>
       <Routes>
         <Route path='/' element={<div>메인페이지</div>} />
         <Route path='/new' element={<div>새글페이지</div>} />
